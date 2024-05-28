41a7771744b20dc38c8d3f7dfb4f43b8e1696c22
jinsolJEONG <122598122+jinsolJEONG@users.noreply.github.com>
2024-05-28 17:35:25 +0900

    Update 팰린드롬 만들기.cc

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,27 +1,44 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-string s, ret; 
-int cnt[200], flag; 
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int cnt[200];
 char mid;
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> s;
-	for(char a : s)cnt[a]++;
-	for(int i = 'Z'; i >= 'A'; i--){
-		if(cnt[i]){
-			if(cnt[i] & 1){
-				mid = char(i);flag++;
-				cnt[i]--;
-			}
-			if(flag == 2)break;
-			for(int j = 0; j < cnt[i]; j += 2){
-				ret = char(i) + ret; 
-				ret += char(i);
-			}
-		}
-	}
-	if(mid)ret.insert(ret.begin() + ret.size() / 2, mid);
-	if(flag == 2)cout << "I'm Sorry Hansoo\n";
-	else cout << ret << "\n"; 
+int flag;
+string rst;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //r
+
+    //i
+    cin >> s;
+    for(int c : s)cnt[c]++;
+    //s
+    for(int i = 'Z'; i >= 'A'; i--){
+        if(cnt[i]){
+            //홀수개인 경우
+            if(cnt[i] & 1){
+                mid = char(i); 
+                flag++;
+                cnt[i]--;
+            }
+            //만약 홀수개가 두개이면 성립안되니까
+            if(flag == 2)break;
+            //짝수개인 것들은 앞뒤로 붙이기
+            for(int j = 0; j < cnt[i]; j += 2){
+                rst = char(i) + rst; //앞
+                rst += char(i); //뒤
+            }            
+        }
+    }
+    //홀수 가운데 끼워넣기
+    if(mid)rst.insert(rst.begin() + rst.size()/2, mid);
+    //o
+    if(flag == 2)cout << "I'm Sorry Hansoo\n";
+    else cout << rst << "\n";
+
+    return 0;
 }

b43ef4205989047ca4c7c4b0a1837cb4ae3526b5
jinsolJEONG <122598122+jinsolJEONG@users.noreply.github.com>
2024-07-16 21:41:57 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+//bfs로 풀어야하는데..
+int n, k;
+const int mx = 100000;
+int times[mx+4];
+int ways[mx+4];
+
+void bfs(){
+    times[n] = 1;
+    ways[n] = 1;
+    queue<int> q;
+    q.push(n);
+    while(!q.empty()){
+        int now = q.front();
+        q.pop();
+        for(int next : {now-1, now+1, now*2}){
+            if(next >= 0 && next <= mx){
+                if(!times[next]){
+                    times[next] = times[now] + 1;
+                    ways[next] = ways[now];
+                    q.push(next);
+                }else if(times[next]==times[now]+1)
+                    ways[next] += ways[now];
+            }
+
+        }
+    }return;
+
+}
+
+int main(){
+    //r
+    //i
+    cin >> n >> k;
+    //s
+    bfs();
+    //o
+    cout << times[k]-1 << '\n' << ways[k];
+    return 0;
+}

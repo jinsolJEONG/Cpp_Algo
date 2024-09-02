7cd7c24e737096014431449d7cac03f4f65ea27e
jinsolJEONG <122598122+jinsolJEONG@users.noreply.github.com>
2024-09-02 21:53:51 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2424 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,40 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std;   
-int a[101][101], e[101][101], visited[101][101], n, temp, ret = 1;
-int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-void dfs(int y, int x, int d){
-    visited[y][x] = 1; 
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int ret = 1;
+int arr[101][101], used[101][101];
+int dy[]={-1,0,1,0}, dx[]={0,1,0,-1};
+
+void dfs(int y, int x, int lev){
+    used[y][x] = 1;
     for(int i = 0; i < 4; i++){
-        int ny = y + dy[i]; 
-        int nx = x + dx[i]; 
-        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue; 
-        if(!visited[ny][nx] && a[ny][nx] > d) dfs(ny, nx, d);
-    }
-    return;
-} 
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny<0||nx<0||ny>=n||nx>=n)continue;
+        if(used[ny][nx]==0 && arr[ny][nx] > lev)
+            dfs(ny, nx, lev);
+    }return;
+}
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);   
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> a[i][j];
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //i
+    cin >> n;
+    for(int i =0; i < n; i++)
+        for(int j= 0; j < n; j++){
+            cin >> arr[i][j];
         }
-    }
-    for(int d = 1; d < 101; d++){
-        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0); 
-        int cnt = 0;  
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(a[i][j] > d && !visited[i][j]) {
-                    dfs(i, j, d);
+    //s
+    for(int l = 1; l <= 100; l++){
+        fill(&used[0][0], &used[0][0]+101*101, 0);
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(used[i][j]==0 && arr[i][j] > l){
+                    dfs(i, j, l);
                     cnt++;
                 }
-            }
-        }
         ret = max(ret, cnt);
     }
+    //o
     cout << ret << '\n';
-    return 0; 
+    return 0;
 }

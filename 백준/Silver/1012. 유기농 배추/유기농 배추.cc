ad981d0f219e360389fe8fbe1cd6c2d45c1b26a1
jinsolJEONG <122598122+jinsolJEONG@users.noreply.github.com>
2024-09-02 21:11:03 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,45 +1,50 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1}; 
-int m, n, k, y, x, ret, ny, nx, t;
-int a[51][51];
-bool visited[51][51]; 
+#include <bits/stdc++.h>
+using namespace std;
+
+int t,n,m,k,x,y;
+int dy[]={-1,0,1,0}, dx[]={0,1,0,-1};
+int a[54][54], used[54][54];
+
 void dfs(int y, int x){
-    visited[y][x] = 1;
+    used[y][x] = 1;
     for(int i = 0; i < 4; i++){
-        ny = y + dy[i];
-        nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny >=n || nx >= m) continue;
-        if(a[ny][nx] == 1 && !visited[ny][nx]){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny<0||nx<0||ny>=n||nx>=m)continue;
+        if(a[ny][nx] ==1 && used[ny][nx]!=1)
             dfs(ny, nx);
-        }
     }
     return;
 }
 
-int main(){ 
-    cin.tie(NULL);
-    cout.tie(NULL);
+
+int main(){
+    //r
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //i
     cin >> t;
     while(t--){
-        fill(&a[0][0], &a[0][0] + 51 * 51, 0);
-        fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
-        ret = 0;
+        int ret = 0;
+        fill(&a[0][0], &a[0][0]+54*54, 0);
+        fill(&used[0][0], &used[0][0]+54*54, 0);
         cin >> m >> n >> k;
-        for(int i = 0; i < k; i++){
+        for(int i =0; i < k; i++){
             cin >> x >> y;
             a[y][x] = 1;
         }
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++){
-                if(a[i][j] == 1 && !visited[i][j]){
-                    dfs(i, j);
+                if(a[i][j]==1 && used[i][j]==0){
+                    dfs(i,j);
                     ret++;
                 }
             }
-        }
-        cout << ret << "\n"; 
+        cout << ret << '\n';
     }
+    //s
+    //o
+    
     return 0;
 }
